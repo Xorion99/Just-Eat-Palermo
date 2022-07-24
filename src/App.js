d10b8f73788408d5563eea38591cea487bba351d
Xorion99 <x81001070@studium.unict.it>
2022-07-24 16:09:13 +0200

    first iteration items

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import React, { Component } from 'react';
 import Navbar from './component/navbar';
 import Card from './component/card';
 import milza from './images/milza.jpg'
+import panelle from './images/panelle.jpeg'
 class App extends Component {
+state = {
+  cards: [
+    {id: 0, img: milza, nome: 'Panino con milza', prezzo: '2,50'},
+    {id: 1, img: panelle, nome:'Panino con le panelle',prezzo: '1,50'}
+  ]
+}
+
   render() {
     return (
       <>
         <Navbar />
+        <div className='conteiner'>
         <h1>Cosa vuoi mangiare</h1>
         <hr></hr>
         <div className='row'>
-          <Card 
-          img = {milza}
-          prezzo = '2,50'
-          />
+        {this.state.cards.map(card =>(<Card
+         key={card.id}
+         img={card.img}
+         titolo={card.nome}
+         prezzo={card.prezzo}
+         />))}
 
+        </div>
         </div>
       </>
     );
--- src/component/navbar.js
+++ src/component/navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component{
   return (
     <nav className="navbar navbar-dark bg-dark">
     <div className="container-fluid">
-      <span className="navbar-brand mb-0 h1">Navbar</span>
+      <span className="navbar-brand mb-0 h1">Just Eat Palermo</span>
     </div>
   </nav>
     );
